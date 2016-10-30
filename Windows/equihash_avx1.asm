8d7b5748547a9d49d4d16fca59142d311efefcfe
Augustin Juricic <ajuricic@neogov.net>
2016-10-30 18:05:00 +0100

    Resolve section conflicts.

--- Windows/equihash_avx1.asm
+++ Windows/equihash_avx1.asm
@@ -7,10 +7,10 @@ include "params.inc"
 include "struct_eh.inc"
 include "macro_eh.asm"
 
-section '.text' align 64
+section '.text1' align 64
 include "proc_ehprepare_avx1.asm"
 include "proc_ehsolver_avx1.asm"
 
-section '.data' writeable align 64
+section '.data1' writeable align 64
 include "data_blake2b.asm"
 testinput file "t2.bin"
--- Windows/equihash_avx2.asm
+++ Windows/equihash_avx2.asm
@@ -7,10 +7,10 @@ include "params.inc"
 include "struct_eh.inc"
 include "macro_eh.asm"
 
-section '.text' align 64
+section '.text2' align 64
 include "proc_ehprepare_avx2.asm"
 include "proc_ehsolver_avx2.asm"
 
-section '.data' writeable align 64
+section '.data2' writeable align 64
 include "data_blake2b.asm"
 testinput file "t2.bin"
